61cf3b8e0f770a16fbcdfd38d244c886e5da10a3
mrpepz <ppepe34@yahoo.com>
2016-05-21 22:19:55 -0500

    Update weather.js

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -285,16 +285,16 @@ CanvasBackground = {
         /* Resize the canvas to occupy the full page, 
            by getting the widow width and height and setting it to canvas*/
          
-        c.weatherCanvas.width  = $('.page-wrap').width;
-        c.weatherCanvas.height = $('.page-wrap').height;
-        c.rainCanvas.width  = $('.page-wrap').width;
-        c.rainCanvas.height = $('.page-wrap').height;
-        c.cloudCanvas.width  = $('.page-wrap').width;
-        c.cloudCanvas.height = $('.page-wrap').height;
-        c.timeCanvas.width  = $('.page-wrap').width;
-        c.timeCanvas.height = $('.page-wrap').height;
-        c.lightningCanvas.width = $('.page-wrap').width;
-        c.lightningCanvas.height = $('.page-wrap').height;
+        c.weatherCanvas.width  = $('.page-wrap').width();
+        c.weatherCanvas.height = $('.page-wrap').height();
+        c.rainCanvas.width  = $('.page-wrap').width();
+        c.rainCanvas.height = $('.page-wrap').height();
+        c.cloudCanvas.width  = $('.page-wrap').width();
+        c.cloudCanvas.height = $('.page-wrap').height();
+        c.timeCanvas.width  = $('.page-wrap').width();
+        c.timeCanvas.height = $('.page-wrap').height();
+        c.lightningCanvas.width = $('.page-wrap').width();
+        c.lightningCanvas.height = $('.page-wrap').height();
     },
 
     chooseBackground: function(condition) {
